750d93851ee0d0700de2a7de750c1317289fd480
Naths <nataliatorreh@gmail,com>
2025-03-18 20:11:30 -0500

    actualizacion de codigo

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAmigos = [];
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById('nombre');
+    let nombreAmigo = inputAmigo.value;
+
+    if (!nombreAmigo) {
+        alert('Debes ingresar un nombre');
+        return;
+    }
+
+    nombresAmigos.push(nombreAmigo);
+    inputAmigo.value = '';
+    inputAmigo.focus();
+    
+}
